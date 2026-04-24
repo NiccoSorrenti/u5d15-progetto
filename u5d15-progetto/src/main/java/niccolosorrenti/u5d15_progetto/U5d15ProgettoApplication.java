a5503f8a948a61bb10be51e49de5a8b3559e8409
Niccolò <niccolo.sorrenti@gmail.com>
2026-04-24 13:02:59 +0200

    Initial commit

---
+++ u5d15-progetto/src/main/java/niccolosorrenti/u5d15_progetto/U5d15ProgettoApplication.java
@@ -0,0 +1,13 @@
+package niccolosorrenti.u5d15_progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5d15ProgettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5d15ProgettoApplication.class, args);
+	}
+
+}
